b527dcd17e65c0a5a937a28db6a147e3836ed793
xunyan <yan.xun@advance.ai>
2020-09-21 10:35:22 +0800

    add file

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,9 +13,10 @@ module.exports = {
       {
         text: "功能介绍",
         items: [
+          { text: "添加资源", link: "/sources/添加任务模块" },
           { text: "测试用例", link: "/sources/test-case" },
-          { text: "HTTP", link: "/sources/http" },
-          { text: "SQL", link: "/sources/sql" },
+          { text: "服务监控", link: "/sources/服务监控" },
+          { text: "权限管理", link: "/sources/权限管理和分享" },
         ],
       },
       { text: "Github", link: "https://github.com/Atome-FE/Mars" },
